88714caa31d3e0346d46a60bdb75c24a6b89c0dd
Nathaniel Nazareno <neonaelnazareno112234@gmail.com>
2024-05-12 20:14:01 +0800

    add comment

--- src/main/java/com/example/Controller/LogInController.java
+++ src/main/java/com/example/Controller/LogInController.java
@@ -23,7 +23,7 @@ public class LogInController {
     public Text actionTarget;
     public PasswordField passwordLoginField;
 
-
+  // qweqewqe
     @FXML
     public void onLoginButtonClick(ActionEvent actionEvent) throws SQLException, IOException {
         String username = usernameField.getText();
